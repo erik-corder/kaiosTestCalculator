8ad11e6d312c7478508b8ad3c1455660919d004f
kanishkaisuru <sampathisuru516@gmail.com>
2019-12-12 13:58:02 +0530

    test

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,7 @@
 var EQUAL = 13, KEY_UP = 38, KEY_RIGHT = 39, KEY_DOWN = 40;
 
+var NUM1 = 0;
+
 
 function main() {
     frames = 0;
@@ -17,6 +19,42 @@ function main() {
 
 function update(){
    if(keystate[EQUAL]) console.log("aaaa")
+}
+
+function getOne(){
+    document.getElementById("val").value = "1";
+}
+
+function getTwo(){
+    document.getElementById("val").value = "2";
+    NUM1 = 2;
+}
+
+function getThree(){
+    document.getElementById("val").value = "3";
+}
+
+function getFour(){
+    document.getElementById("val").value = "4";
+}
+
+function getFive(){
+    document.getElementById("val").value = "5";
+}
+function getSix(){
+    document.getElementById("val").value = "6";
+}
+
+function getSeven(){
+    document.getElementById("val").value = "7";
+}
+
+function getEight(){
+    document.getElementById("val").value = "8";
+}
+
+function getNine(){
+    document.getElementById("val").value = "9";
 }
 
 main();
